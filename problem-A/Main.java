5633d1ffdb0069f9a44ef8dd47a1ca6f8ba6bf59
Alimkulov Nurbol <124434364+VectorFayton@users.noreply.github.com>
2023-02-11 12:40:48 +0600

    Create README.md

--- problem-A/Main.java
+++ problem-A/Main.java
@@ -1,8 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-
+        Animal pig = new Animal("Pig");
+        Mammal bober = new Mammal("Bober");
+        Cat Barsik = new Cat("Barsik");
         Dog Ruslan = new Dog("Ruslan");
+        System.out.println(pig.toString());
+        System.out.println(Barsik.toString());
         System.out.println(Ruslan.toString());
         Ruslan.greets();
+        Ruslan.greets(Dog another);
     }
 }
